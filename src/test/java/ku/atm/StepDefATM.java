62dac5523e2bce05fd66deb4a645e86ee2d023dc
TheBunny737 <teerapat.d@ku.th>
2023-11-28 18:03:20 +0700

    Lab 10 6310451201 Teerapat

--- src/test/java/ku/atm/StepDefATM.java
+++ src/test/java/ku/atm/StepDefATM.java
@@ -49,6 +49,11 @@ public class StepDefATM {
         atm.withdraw(amount);
     }
 
+    @When("I deposit {float}")
+    public void i_deposit_balance(double amount)  {
+        atm.deposit(amount);
+    }
+
     @When("I overdraw {float} from ATM")
     public void i_withdraw_from_atm_more_than_balance(double amount) throws NotEnoughBalanceException {
         assertThrows(NotEnoughBalanceException.class,
